9bdeec3bd7314993360dddabaf7e890f16ed58f6
Gioianac <joy.oppliger@heig-vd.ch>
2019-06-03 23:38:40 +0200

    Update filters component

--- resources/js/components/Filters/Filters.js
+++ resources/js/components/Filters/Filters.js
@@ -1,15 +1,20 @@
-    export default {
-        data() {
-            return {
-                products : []
-            }
-        },
-        props : ['prod'],
-        mounted () {
-            let json = JSON.parse(this.prod);
-            this.products = json.products;
-        },
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
 
-    }
+export default {
+    data() {
+        return {
+            products : [],
+            value_2: [10.77, 915.45]
+        }
+    },
+    components: {
+        VueSlider
+    },
+    props : ['prod'],
+    mounted () {
+        let json = JSON.parse(this.prod);
+        this.products = json.products;
+    },
 
-
+}
