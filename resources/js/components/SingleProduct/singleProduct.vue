1f1758da00c3e7b25fa056eeb23feddfb5d4358f
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-06-03 21:17:13 +0200

    stylisation error message single product

--- resources/js/components/SingleProduct/singleProduct.vue
+++ resources/js/components/SingleProduct/singleProduct.vue
@@ -36,13 +36,16 @@
                                                 <select class="choice_list" v-model="quantity">
                                                         <option>10000</option>
                                                 </select>
-                                                    <p v-if="errors.quantity">{{errors.quantity[0]}}</p>
+                                                   
                                                 </div>
+
                                                 <div @click="input(product.id)" class="product_button add_product text-center d-flex flex-column align-items-center justify-content-center">AJOUTER</div>
                                                 <div class="product_button product_cart text-center d-flex flex-column align-items-center justify-content-center">
                                                 <div><div><img src="images/cart.svg" class="svg" alt=""></div></div>
                                             </div>
                                         </div>
+                                                                                     <p class="error pt-2" v-if="errors.quantity">{{errors.quantity[0]}}</p>
+
                                     </form>
                                 </div>
                                 <div class="selection">
