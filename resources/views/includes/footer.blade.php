65a8bc38cdfea3a54b8a7715ba5689611a959cb0
Ksenia <ksenia.mironova.95@gmail.com>
2019-06-03 15:56:27 +0200

    adding a navigation bar component with new routes

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,95 @@
+<!-- Footer -->
+<footer class="page-footer font-small bg-light pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+        <!-- Links -->
+        <h5 class="text-uppercase">Actualités</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">S’inscrire à la newsletter</a>
+          </li>
+          <li>
+            <a href="#!">Presse</a>
+          </li>
+          <li>
+            <a href="#!">Catalogue</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+        <!-- Links -->
+        <h5 class="text-uppercase">Aide</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Contact</a>
+          </li>
+          <li>
+            <a href="#!">FAQ</a>
+          </li>
+          <li>
+            <a href="#!">Conditions générales</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+        <!-- Links -->
+        <h5 class="text-uppercase">À propos</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Qui sommes-nous ?</a>
+          </li>
+          <li>
+            <a href="#!">Plan d’accès</a>
+          </li>
+          <li>
+            <a href="#!">Partenaires</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">GAZZAR SA</h5>
+
+        <p>Chemin du Dévent  1024 <br>Ecublens, Suisse</p>
+        <p>tél : 021 691 86 71 <br> elie@gazzar.ch</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center  py-3">
+    <p>© 2019 Gazzar SA</p>
+
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ resources/views/includes/main-navbar.blade.php
@@ -0,0 +1,78 @@
+<div class="navbar-text ">
+  <a href="#">Inscription à la newsletter</a>
+</div>
+<nav  class="navbar navbar-expand-md navbar-light shadow-sm ">
+    <!-- <div class="container"> -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <!-- {{ config('app.name', 'Gazzar') }} -->
+            <img src="{{ asset('images/gazzar-logo.svg') }} " width="200" height="auto" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Languages -->
+                <li class="nav-item dropdown">
+                  <img src="{{ asset('images/select-language-button.svg') }} " width="25" height="25" alt="Languages" >
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    FR
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                     <a class="dropdown-item">EN</a> <!-- href="https://www.gazzar-wines.ch/" -->
+                     <div class="dropdown-divider"></div>
+                     <a class="dropdown-item" >DE</a> <!--  href="https://www.gazzar-weine.ch/" -->
+                  </div>
+                </li>
+                <!-- Authentication Links -->
+          @guest
+          <li class="nav-item dropdown">
+            <img src="{{ asset('images/user.svg') }} " width="25" height="25" alt="Login" >
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MON COMPTE</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
+              <div class="dropdown-divider"></div>
+                @if (Route::has('register'))
+              <a class="dropdown-item" href="{{ route('register') }}">{{ __('Registrer') }}</a>
+            </div>
+          </li>
+          @endif
+
+          @else
+          <li class="nav-item dropdown">
+            <img src="{{ asset('images/user.svg') }} " width="25" height="25" alt="Login" >
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+
+                  {{ Auth::user()->username }} <span class="caret"></span>
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+              </div>
+          </li>
+          @endguest
+            <li class="nav-item">
+              <img src="{{ asset('images/favorite-heart-white.svg') }} " width="25" height="25" alt="Mes Préférés" >
+              <a class="nav-link" href="#" >MES PRÉFÉRÉS</a>
+           </li>
+           <li class="nav-item">
+            <img src="{{ asset('images/shopping-cart.svg') }} " width="25" height="25" alt="Login" ></a>
+            <a class="nav-link" href="#">PANIER</a>
+          </li>
+        </ul>
+    </div>
+<!-- </div> -->
+</nav> <!--  end of a principal nav -->
