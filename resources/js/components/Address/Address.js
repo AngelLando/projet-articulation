84af4c9e31f455054703e3b9e2f9603d8202a16e
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-06-04 12:04:39 +0200

    prepared adress form

--- resources/js/components/Address/Address.js
+++ resources/js/components/Address/Address.js
@@ -1,44 +1,95 @@
 export default {
-    data() {
-        return {
-            address : '',
-            firstname: '',
-            lastname : '',
-            gender : '',
-            street : '',
-            npa : '',
-            city : '',
-            region : '',
-            country : '',
-        }
-    },
+  data() {
+    return {
+      address1 : '',
+      firstname1: '',
+      lastname1 : '',
+      gender1 : '',
+      street1 : '',
+      npa1 : '',
+      city1 : '',
+      region1 : '',
+      country1 : '',
+      address2 : '',
+      firstname2: '',
+      lastname2 : '',
+      gender2 : '',
+      street2 : '',
+      npa2 : '',
+      city2 : '',
+      region2 : '',
+      country2 : '',
+      address3 : '',
+      firstname3: '',
+      lastname3 : '',
+      gender3 : '',
+      street3 : '',
+      npa3 : '',
+      city3 : '',
+      region3 : '',
+      country3 : '',
+      isHiddenShipTo:false,
+      isHiddenBillTo:false,
+    }
+  },
     //props : ['prod'],
     mounted () {
 
     },
     methods : {
-      submitAddress (e) {
+      submitAddress (isHiddenBillTo,isHiddenShipTo) {
         //  e.preventDefault();
-          this.address  = {
-              firstname: this.firstname,
-              lastname: this.lastname,
-              gender: this.gender,
-              street: this.street,
-              npa: this.npa,
-              city: this.city,
-              region: this.region,
-              country: this.country,
-          };
-          this.data = {
-              address : this.address,
-              products : JSON.parse(this.cart),
-          }
-          console.log(this.data)
-          axios.post('check', this.data).then(console.log('formulaire envoyé!')).then(function (response) {
-              console.log(response);
-          })
-      }
+        this.address1  = {
+          firstname1: this.firstname1,
+          lastname1: this.lastname1,
+          gender1: this.gender1,
+          street1: this.street1,
+          npa1: this.npa1,
+          city1: this.city1,
+          region1: this.region1,
+          country1: this.country1,
+        };
+        if (isHiddenShipTo) {
+        this.address2  = {
+          firstname2: this.firstname2,
+          lastname2: this.lastname2,
+          gender2: this.gender2,
+          street2: this.street2,
+          npa2: this.npa2,
+          city2: this.city2,
+          region2: this.region2,
+          country2: this.country2,
+        };}
+        else{
+          this.address2 = null;
+        }
+                if (isHiddenBillTo) {
+        this.address3  = {
+          firstname3: this.firstname3,
+          lastname3: this.lastname3,
+          gender3: this.gender3,
+          street3: this.street3,
+          npa3: this.npa3,
+          city3: this.city3,
+          region3: this.region3,
+          country3: this.country3,
+        };}
+        else{
+          this.address3=null;
+        }
+        this.data = {
+          address1 : this.address1,
+          address2 : this.address2,
+          address3 : this.address3,
+          products : JSON.parse(this.cart),
+        }
+        console.log(this.data)
+        axios.post('check', this.data).then(console.log('formulaire envoyé!')).then(function (response) {
+          console.log(response);
+        })
+      },
+
     },props : ['cart'],
 
-}
+  }
 
