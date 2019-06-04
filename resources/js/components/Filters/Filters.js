06598f13a078bfe5a061c32edb2996c37758cca4
Gioianac <joy.oppliger@heig-vd.ch>
2019-06-04 07:52:49 +0200

    Add bootstrap-vue

--- resources/js/components/Filters/Filters.js
+++ resources/js/components/Filters/Filters.js
@@ -1,15 +1,28 @@
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/antd.css'
+import { BFormGroup } from 'bootstrap-vue/es/components';
+import { BFormCheckboxGroup } from 'bootstrap-vue/es/components';
+import { BFormCheckbox } from 'bootstrap-vue/es/components';
 
 export default {
     data() {
         return {
             products : [],
-            value_2: [10.77, 915.45]
+            value_2: [10.77, 915.45],
+            selected: [], // Must be an array reference!
+            options: [
+                { text: 'Orange', value: 'orange' },
+                { text: 'Apple', value: 'apple' },
+                { text: 'Pineapple', value: 'pineapple' },
+                { text: 'Grape', value: 'grape' }
+            ]
         }
     },
     components: {
-        VueSlider
+        VueSlider,
+        BFormGroup,
+        BFormCheckboxGroup,
+        BFormCheckbox
     },
     props : ['prod'],
     mounted () {
