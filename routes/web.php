200b6b1649253b81eac9d1201165e631c7ae38ba
Aksumiron <35137311+Aksumiron@users.noreply.github.com>
2019-06-04 13:11:30 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,9 @@ Auth::routes();
 // GET
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
-=======
-
 
 //=> JSON file
->>>>>>> header
+
 Route::get('/products', 'ProductController@show');
 
 Route::get('/produit/{id}', [
