ada50e263fc99aabc792fe3a9a8ce2c8bbbc717b
Gioianac <joy.oppliger@heig-vd.ch>
2019-06-05 10:47:25 +0200

    Add bigCatalog on page nos-vins and mini on homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,6 @@
 @section('content')
     <div id="app">
         <slider-homepage></slider-homepage>
-        <big-catalog prod="{{$products}}"></big-catalog>
+        <mini-products-catalog prod="{{$products}}"></mini-products-catalog>
     </div>
 @endsection
