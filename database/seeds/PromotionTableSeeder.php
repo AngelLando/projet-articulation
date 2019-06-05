fa64354052099b953bbcde18b75008b1d9e9b13c
Angel Lando <angel.lando@outlook.com>
2019-06-05 18:57:22 +0200

    Improved product management system validation

--- database/seeds/PromotionTableSeeder.php
+++ database/seeds/PromotionTableSeeder.php
@@ -12,6 +12,7 @@ class PromotionTableSeeder extends Seeder
     public function run()
     {
         DB::table('promotions')->insert([
+            ['type'=> 'Aucune', 'amount' => 0],
             ['type'=> 'Offre de fin de lot', 'amount' => 10],
             ['type' => 'Offre spéciale', 'amount' => 20],
         ]);
