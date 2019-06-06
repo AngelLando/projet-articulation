269e50d2b48e9f56e0fbf02bd9f07592c78b38ec
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-06-06 11:15:42 +0200

    finalised localstorage for single product and cart

--- resources/js/components/Cart/Cart.js
+++ resources/js/components/Cart/Cart.js
@@ -14,11 +14,11 @@ export default {
 	methods:{
 		underline: function(event){
 		},
-        deleteProduct : function (event) {
+		deleteProduct : function (event) {
 			axios.delete('cartItem/' + event.id).catch(error => {
 				console.dir(error);
 			})
-        }
+		}
 	},
 
 	computed:{
@@ -31,26 +31,29 @@ export default {
 	props : ['cart'],
 	mounted () {
 		/*
-		this.products = JSON.parse(this.cart);
-		var finalsubPrice= 0;
 				
+		*/
+		let id = document.querySelector("meta[name='user-id']")
+		if (id != null) {
+			console.log("test")
+			this.products = JSON.parse(this.cart);
+
+		} if(id == null) {
+			console.log("d")
+			this.products = JSON.parse(localStorage.getItem('storedID'));
+			console.log(this.products)
+		}
+		var finalsubPrice= 0;
 		this.products.forEach(function(product) {
 			var total =  product.price*product.packaging_capacity;
 			product.totalprice=total
 			finalsubPrice=finalsubPrice+product.totalprice;
 		});
 		this.finalsubPrice=finalsubPrice;
-	this.finalPrice = this.finalsubPrice+this.tva+this.livraison;*/
-		let id = document.querySelector("meta[name='user-id']")
-		if (id != null) {
-			console.log(id.getAttribute('content'))
-		} if(id == null) {
-			console.log('rien trouvé ')
-		}
+		this.tva = Math.round(this.tvaPercent*this.finalsubPrice/100);
+		this.finalPrice = this.finalsubPrice+this.tva+this.livraison;
 	},
 	beforeMount(){
-		this.product.totalprice=this.product.price*this.product.quantity;
-				//this.tva = Math.round(this.tvaPercent*this.this.product.totalprice/100);
 
 
 
