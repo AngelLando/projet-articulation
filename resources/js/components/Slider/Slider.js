871ceb0605ebd073018ca87818a19b22218acfb4
Gioianac <joy.oppliger@heig-vd.ch>
2019-06-07 10:24:19 +0200

    Improve the slider

--- resources/js/components/Slider/Slider.js
+++ resources/js/components/Slider/Slider.js
@@ -1,5 +1,15 @@
 export default {
 
 
+    data() {
+        return {
+            showSlide1: true,
+            showSlide2: false,
+            showSlide3: false,
+        }
+    },
+
+
 }
+
 
--- resources/js/components/Slider/Slider.vue
+++ resources/js/components/Slider/Slider.vue
@@ -3,30 +3,36 @@
         <section class="slider">
             <div>
                 <!--<div class="slider"><img class="image-slider" src="images/slider-image1.jpg" alt=""></div>!-->
-                <div id="slide1" class="slider-image active" style="background-image:url(images/slider-image1.jpg)">
+                <div id="slide1" class="slider-image active" style="background-image:url(images/slider-image1.png)" v-if="showSlide1">
                     <div class="row row-image">
-                        <div class="col-xl-6 col-lg-7 infos">
-                            <h2>8 juin 2019</h2>
-                            <p>Dégustation de vins italiens dans les montagnes neuchâteloises.</p>
-                            <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Inscription</a></button>
+                        <div class="col-md-4 p-0 infos">
+                            <div class="infos-inside">
+                                <h2>8 juin 2019</h2>
+                                <p class="descr">Dégustation de vins italiens dans les montagnes neuchâteloises.</p>
+                                <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Inscription</a></button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div id="slide2" class="slider-image" style="background-image:url(images/slider-image1.jpg)">
+                <div id="slide2" class="slider-image" style="background-image:url(images/slider-image2.png)" v-if="showSlide2">
                     <div class="row">
-                        <div class="col-xl-6 col-lg-7 infos">
-                            <h2>Jusqu'au 30 juin</h2>
-                            <p>Profitez de réductions exceptionnelles sur les offres du mois !</p>
-                            <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Découvrir</a></button>
+                        <div class="col-md-4 p-0 infos">
+                            <div class="infos-inside">
+                                <h2>Jusqu'au 30 juin</h2>
+                                <p class="descr">Profitez de réductions exceptionnelles sur les offres du mois !</p>
+                                <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Découvrir</a></button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div id="slide3" class="slider-image" style="background-image:url(images/slider-image1.jpg)">
+                <div id="slide3" class="slider-image" style="background-image:url(images/slider-image3.png)" v-if="showSlide3">
                     <div class="row">
-                        <div class="col-xl-6 col-lg-7 infos">
-                            <h2>Les favoris</h2>
-                            <p>Découvrez les vins qui rencontrent le plus de succès dans notre catalogue.</p>
-                            <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Découvrir</a></button>
+                        <div class="col-md-4 p-0 infos">
+                            <div class="infos-inside">
+                                <h2>Les favoris</h2>
+                                <p class="descr">Découvrez les vins qui rencontrent le plus de succès dans notre catalogue.</p>
+                                <button class="btn-primary btn-inscription"><a href="#" class="site-btn sb-line">Découvrir</a></button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,17 +43,17 @@
                 <div class="row slider-options">
                     <div class="col-md-4 p-0 feature active">
                         <div id="btn-slide-1" class="feature-inner">
-                            <h2><a href="">DÉGUSTATION</a></h2>
+                            <h2 v-on:click="showSlide1=true; showSlide2=false; showSlide3=false">DÉGUSTATION</h2>
                         </div>
                     </div>
                     <div id="btn-slide-2" class="col-md-4 p-0 feature">
                         <div class="feature-inner">
-                            <h2><a href="">OFFRES DU MOIS</a></h2>
+                            <h2 v-on:click="showSlide1=false; showSlide2=true; showSlide3=false">OFFRES DU MOIS</h2>
                         </div>
                     </div>
                     <div id="btn-slide-3" class="col-md-4 p-0 feature">
                         <div class="feature-inner">
-                            <h2><a href="">COUPS DE COEUR</a></h2>
+                            <h2 v-on:click="showSlide1=false; showSlide2=false; showSlide3=true">COUPS DE COEUR</h2>
                         </div>
                     </div>
                 </div>
@@ -63,5 +69,6 @@
 
 <script src="./Slider.js"></script>
 <style src="./Slider.css" scoped></style>
+
 
 
