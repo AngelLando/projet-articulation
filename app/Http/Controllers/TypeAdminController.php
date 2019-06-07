70e2ed37e21f337743a7b5b43b853c8f6aef4827
Angel Lando <angel.lando@outlook.com>
2019-06-07 09:12:14 +0200

    Finished type management system

---
+++ app/Http/Controllers/TypeAdminController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Session;
+use App\Type;
+use Illuminate\Http\Request;
+
+class TypeAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.types.index')->with('types', Type::orderBy('created_at', 'desc')->paginate(5));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.types.create')->with('types', Type::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'type' => 'required'
+        ]);
+
+        $type = Type::create([
+            'name' => $request->type
+        ]);
+
+        Session::flash('success', 'Le type a été créé!');
+
+        return redirect()->route('types.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $type = Type::find($id);
+        return view('admin.types.edit')->with('type', $type);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'type' => 'required'
+        ]);
+
+        $type = Type::find($id);
+        $type->name = $request->type;
+        $type->save();
+
+        Session::flash('success', 'Type mis à jour!');
+
+        return redirect()->route('types.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Type::destroy($id);
+
+        Session::flash('success', 'Le type a été supprimé!');
+
+        return redirect()->route('types.index');
+    }
+}
---
+++ resources/views/admin/types/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container"><br>
+@if(Session::has('success'))
+    <div class="alert alert-success">
+        {{ Session::get('success') }}
+    </div>
+@endif
+
+    <div class="card">
+        <div class="card-header">
+            Edition: {{ $type->name }}
+        </div>
+
+        <div class="card-body">
+            <form action="{{ route('types.update', ['id' => $type->id]) }}" method="POST">
+                {{ csrf_field() }}
+                {{ method_field('PUT') }}
+                <div class="form-group">
+                    <div class="form-row">
+                        <div class="col-12">
+                            <label for="type">Type</label>
+                            <input type="text" name="type" class="form-control {{ $errors->has('type') ? 'is-invalid' : '' }}" value="{{ $type->name }}" placeholder="Type">
+                            @if ($errors->has('type'))
+                            <div class="invalid-feedback">{{ $errors->first('type') }}</div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="text-center">
+                        <button class="btn btn-primary" type="submit">Mettre à jour le type</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div><br>
+@endsection
