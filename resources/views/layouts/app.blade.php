1f19a1dd9fad069b1835ea059438d8fd45c91256
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-06-08 12:15:37 +0200

    minor css changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+        <link rel="stylesheet" href="{{asset('css/login.css')}}">
+
 
 </head>
 <body>
