0c9a3053a30637e7dc54882ca64c12878113369f
Christophe Schranz <christophe.schranz@heig-vd.ch>
2019-06-09 14:10:49 +0200

    Homepage products limited to recommandations only

--- database/migrations/2019_05_28_094540_create_product_ratings_table.php
+++ database/migrations/2019_05_28_094540_create_product_ratings_table.php
@@ -16,14 +16,14 @@ class CreateProductRatingsTable extends Migration
         Schema::create('product_ratings', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('value');
-            $table->string('comment_subject');
-            $table->string('comment_description');
+            $table->string('comment_subject')->nullable();
+            $table->string('comment_description')->nullable();
             
-            $table->integer('user_id')->unsigned()->unique();
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users');
-            $table->integer('product_id')->unsigned()->unique();
+            $table->integer('product_id')->unsigned();
             $table->foreign('product_id')
                 ->references('id')
                 ->on('products')
