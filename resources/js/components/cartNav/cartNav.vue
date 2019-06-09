d9ab2f57aad3f6befe7cdf361712c38c7b829a96
Christophe Schranz <christophe.schranz@heig-vd.ch>
2019-06-09 18:51:12 +0200

    Improvement for hover

--- resources/js/components/cartNav/cartNav.vue
+++ resources/js/components/cartNav/cartNav.vue
@@ -3,13 +3,14 @@
     <li class="nav-item" @mouseover="getCart()" @mouseleave="hover = false">
         <img src="images/shopping-cart.svg" class="icon" width="23" height="23" alt="Panier">
         <a class="nav-link" :href="url">PANIER</a>
+        <div v-if="hover == true" style="display : block; position: absolute; background-color: white; z-index: 400000; ">
+            <ul v-for="cartItem in cart">
+                <p>{{cartItem.name}}</p>
+                <p>{{cartItem.quantity}}</p>
+            </ul>
+        </div>
     </li>
-    <div v-if="hover == true" style="display : block; position: absolute; background-color: white; z-index: 400000; ">
-        <ul v-for="cartItem in cart">
-            <p>{{cartItem.name}}</p>
-            <p>{{cartItem.quantity}}</p>
-        </ul>
-    </div>
+
     </div>
 </template>
 
