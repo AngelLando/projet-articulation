69482b4d505a0f855298669b2a93c1d67547ff6d
Gioianac <joy.oppliger@heig-vd.ch>
2019-06-11 14:19:04 +0200

    Add catalog PDF

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -19,7 +19,7 @@
             <a href="#!">Presse</a>
           </li>
           <li>
-            <a href="#!">Catalogue</a>
+            <a href="/public/uploads/catalogue_gazzar.pdf" target="_blank">Catalogue</a>
           </li>
         </ul>
       </div>
@@ -50,7 +50,7 @@
             <a href="{{route('a-propos')}}">Qui sommes-nous ?</a>
           </li>
           <li>
-            <a href="/public/uploads/plan_gazzar_fr.pdf" download>Plan d’accès</a>
+            <a href="/public/uploads/plan_gazzar_fr.pdf" target="_blank">Plan d’accès</a>
           </li>
           <li>
             <a href="#!">Partenaires</a>
