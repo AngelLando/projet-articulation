767669d695e71ba334d5cb68d55c3a2d95e50b14
Christophe Schranz <christophe.schranz@heig-vd.ch>
2019-06-13 10:35:34 +0200

    Cart added to Catalogs

--- resources/js/components/SingleProduct/singleProduct.js
+++ resources/js/components/SingleProduct/singleProduct.js
@@ -122,6 +122,7 @@ export default {
 	props : ['prod'],
 	mounted () {
 		let json = JSON.parse(this.prod);
+		console.log(json);
 		this.product = json.product;
 		this.products = json.recommandations;
 		this.products = json.products;
